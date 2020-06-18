45d18d1ca464ba43f9c914c0bd3fd33961279b2c
Michaelstanden <michaelstanden@hotmail.com>
2020-06-18 15:32:26 +0000

    commented code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,10 @@
+/**
+ * jquery code. This section invokes the document to be ready.
+ * guide button code in js is used to say when you click on the button 
+ * the intructions will drop down at a medium pace.
+ * Restart button is for the game page to reload and refresh the page 
+ * if players want to restart their game if they are not happy with their score**/
+
 $(document).ready(function () {
     $(".guide-btn").click(function () {
         $(".text-box").toggle("medium");
@@ -6,10 +13,8 @@ $(document).ready(function () {
         location.reload();
     });
 });
-
+/**variables of my game to assign values to objects to invoke and store functions**/
 const cards = document.querySelectorAll(".card");
-//variables of first card to be clicked and value or cards, 
-//second card to be clicked and values and if a card has been flipped
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
@@ -28,7 +33,13 @@ let score = {
 
 
 
-//function to check if the card flipped should lock the board on a no match or should play on and match
+/**function for flipping the card. If you have flipped one card it
+ should not lock the board to allow you to continue a second click
+ if there is a match then the board will not lock as well
+ if there isnt a match then the board will lock until there is a timeout function performed to allow 
+ the logic to remain in place and not crash.
+ contains code for score depletion for each flip the player has performed.
+ contains code for each time a player flips a card it increases the number by one**/
 function flipCard(){
     if(lockBoard) return;
     if(this === firstCard) return;
@@ -53,7 +64,7 @@ this.classList.add('flip');
  }
 
 
-// function to chec for the match. if the first card value data = second value card data they will match
+// function to check for the match. If the first card value data = second value card data they will match
 //if no match then unflip the cards
 function checkForMatch() {
  if (firstCard.dataset.name === secondCard.dataset.name) {
@@ -66,15 +77,17 @@ function checkForMatch() {
  unFlipcards()
 }   
 }
-//function to disable cards if they have matched.
+/**function to disable cards if they have matched. 
+Meaning when cards match they will be disabled to turn back over or be able to be clicked on again
+They will face turned up for the rest of the game until completion or a game reset**/
 function disableCards() {
      firstCard.removeEventListener('click' , flipCard);
     secondCard.removeEventListener('click' , flipCard);
 
     resetBoard();
 }
-//function to set a time out if cards do not match.
-// so they are allowed to be flipped over for the player.
+/**function to set a time out if cards do not match.
+**/
 function unFlipcards() {
     lockBoard = true;
 
@@ -91,14 +104,17 @@ function resetBoard(){
     [firstCard, secondCard] = [null, null];
 }
 
+/**iife (immediately invoked function expression. Means the function will be invoked right after its definition.
+ * This is used so as soon as the page loads the game will shuffle the cards at random
+ *  meaning the player will not have the same board each time**/
 (function shuffleCards(){
     cards.forEach(card => {
         let shuffleRandom = Math.floor(Math.random() * 16);
         card.style.order = shuffleRandom;
     });
 })()
-//iife (immediately invoked function expression. Means the function will be invoked right after its definition.
 
+/**function to invoke overlay text when player has matched all the cards together in pairs */
 function congratulations() {
         winner.classList.add("visible");
     }
