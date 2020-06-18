821e1c9df77d124d73bc47d98d5daadfdbb3bd9a
Michaelstanden <michaelstanden@hotmail.com>
2020-06-18 22:35:32 +0000

    cleaned up code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,12 +6,12 @@
  * if players want to restart their game if they are not happy with their score**/
 
 $(document).ready(function () {
-    $(".guide-btn").click(function () {
-        $(".text-box").toggle("medium");
-    });
-    $(".restart-btn").click(function () {
-        location.reload();
-    });
+	$(".guide-btn").click(function () {
+		$(".text-box").toggle("medium");
+	});
+	$(".restart-btn").click(function () {
+		location.reload();
+	});
 });
 /**variables of my game to assign values to objects to invoke and store functions**/
 const cards = document.querySelectorAll(".card");
@@ -21,16 +21,15 @@ let firstCard, secondCard;
 let matches = 0;
 let winner = document.querySelector(".overlay-text");
 let flip = {
-        flipCounter: document.getElementById("turns-counter"),
-        flipCount: 0,
-        timesFlipped: 0,
-    };
+	flipCounter: document.getElementById("turns-counter"),
+	flipCount: 0,
+	timesFlipped: 0,
+};
 let score = {
-    scoreCounter: document.getElementById("score"),
-    scoreCount: 500,
-    scoreDeducted: 0,
+	scoreCounter: document.getElementById("score"),
+	scoreCount: 500,
+	scoreDeducted: 0,
 }
-
 
 
 /**function for flipping the card. If you have flipped one card it
@@ -40,85 +39,84 @@ let score = {
  the logic to remain in place and not crash.
  contains code for score depletion for each flip the player has performed.
  contains code for each time a player flips a card it increases the number by one**/
-function flipCard(){
-    if(lockBoard) return;
-    if(this === firstCard) return;
-    flip.flipCount++;
-    flip.flipCounter.innerText = flip.flipCount;
-    flip.timesFlipped++;
-    score.scoreCount--;
-    score.scoreCounter.innerText = score.scoreCount;
-    score.scoreDeducted--;
+function flipCard() {
+	if (lockBoard) return;
+	if (this === firstCard) return;
+	flip.flipCount++;
+	flip.flipCounter.innerText = flip.flipCount;
+	flip.timesFlipped++;
+	score.scoreCount--;
+	score.scoreCounter.innerText = score.scoreCount;
+	score.scoreDeducted--;
 
-this.classList.add('flip');
+	this.classList.add('flip');
 
- if (!hasFlippedCard) {
-     //first time a player has clicked a card
-     hasFlippedCard = true;
-     firstCard = this;
-     return;
- } 
-   //second card clicked  
- secondCard = this;
-     checkForMatch();
- }
+	if (!hasFlippedCard) {
+		//first time a player has clicked a card
+		hasFlippedCard = true;
+		firstCard = this;
+		return;
+	}
+	//second card clicked  
+	secondCard = this;
+	checkForMatch();
+}
 
 
 // function to check for the match. If the first card value data = second value card data they will match
 //if no match then unflip the cards
 function checkForMatch() {
- if (firstCard.dataset.name === secondCard.dataset.name) {
-     matches +=1;
-     if(matches === 8){
-         congratulations();
-     }
-    disableCards()
-} else {
- unFlipcards()
-}   
+	if (firstCard.dataset.name === secondCard.dataset.name) {
+		matches += 1;
+		if (matches === 8) {
+			congratulations();
+		}
+		disableCards()
+	} else {
+		unFlipcards()
+	}
 }
 /**function to disable cards if they have matched. 
 Meaning when cards match they will be disabled to turn back over or be able to be clicked on again
 They will face turned up for the rest of the game until completion or a game reset**/
 function disableCards() {
-     firstCard.removeEventListener('click' , flipCard);
-    secondCard.removeEventListener('click' , flipCard);
+	firstCard.removeEventListener('click', flipCard);
+	secondCard.removeEventListener('click', flipCard);
 
-    resetBoard();
+	resetBoard();
 }
 /**function to set a time out if cards do not match.
-**/
+ **/
 function unFlipcards() {
-    lockBoard = true;
+	lockBoard = true;
 
-    setTimeout(()=> {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-    resetBoard();
-}, 1000); 
+	setTimeout(() => {
+		firstCard.classList.remove('flip');
+		secondCard.classList.remove('flip');
+		resetBoard();
+	}, 1000);
 
 }
 
-function resetBoard(){
-    [hasFlippedCard,lockBoard] = [false,false];
-    [firstCard, secondCard] = [null, null];
+function resetBoard() {
+	[hasFlippedCard, lockBoard] = [false, false];
+	[firstCard, secondCard] = [null, null];
 }
 
 /**iife (immediately invoked function expression. Means the function will be invoked right after its definition.
  * This is used so as soon as the page loads the game will shuffle the cards at random
  *  meaning the player will not have the same board each time**/
-(function shuffleCards(){
-    cards.forEach(card => {
-        let shuffleRandom = Math.floor(Math.random() * 16);
-        card.style.order = shuffleRandom;
-    });
+(function shuffleCards() {
+	cards.forEach(card => {
+		let shuffleRandom = Math.floor(Math.random() * 16);
+		card.style.order = shuffleRandom;
+	});
 })()
 
 /**function to invoke overlay text when player has matched all the cards together in pairs */
 function congratulations() {
-        winner.classList.add("visible");
-    }
+	winner.classList.add("visible");
+}
 
-         
+
 cards.forEach(card => card.addEventListener('click', flipCard));
-
